61629847d1a5370839f0bcab4f6a5852302f7d63
chou <chenyuhao@janime.cn>
2025-06-05 12:05:26 +0800

    feat: 客户端适配graphql

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-安装 ESLint
+## 安装 ESLint
 pnpm i eslint eslint-config-airbnb eslint-config-airbnb-typescript eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks @typescript-eslint/parser @typescript-eslint/eslint-plugin -D
 
-vite 支持 eslint
+## vite 支持 eslint
 pnpm i vite-plugin-eslint -D
+
+## 实现 graphql service 层
+pnpm i @apollo/client graphql -S
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useMutation, useQuery } from '@apollo/client'
+import { FIND, UPDATE } from './graphql/demo'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('');
+  const [desc,setDesc] = useState('')
+  
+  const { loading, data } = useQuery(FIND, {
+    variables: {
+      id:'2000630e-4f1f-4d47-8bdc-bcf5b845a61a'
+    }
+  })
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [update] = useMutation(UPDATE)
+  const onChangeNameHandler = (v: React.ChangeEvent<HTMLInputElement>) => {
+    setName(v.target.value);
+  }
+
+  const onChangeDescHandler =  (v: React.ChangeEvent<HTMLInputElement>) => {
+    setDesc(v.target.value);
+  }
+
+  const onClickHandle = () => {
+    update({
+      variables: {
+        id: '2000630e-4f1f-4d47-8bdc-bcf5b845a61a',
+        params: {
+          id: '2000630e-4f1f-4d47-8bdc-bcf5b845a61a',
+          name,
+          desc,
+        }
+  }})
+}
+
+
+  return (<div><p>data:{JSON.stringify(data)}</p>
+    <p>loading:{`${loading}`}</p>
+    
+    <p>
+      name:
+      <input type="text" onChange={onChangeNameHandler} />
+    </p>
+      <p>
+      desc:
+      <input type="text" onChange={onChangeDescHandler} />
+    </p>
+    <p><button onClick={onClickHandle}>更新</button></p>
+  </div>)
 }
 
 export default App
