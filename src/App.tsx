b13cdcdb2c7ee6324f1e515db5aa5e811d025815
chou <1070549372@qq.com>
2025-06-06 22:38:49 +0800

    feat: 添加antd-mobile UI库

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { useMutation, useQuery } from '@apollo/client'
 import { FIND, UPDATE } from './graphql/demo'
 import { useState } from 'react'
+import { Button, Form, Input } from 'antd-mobile';
 
 function App() {
   const [name, setName] = useState('');
@@ -14,22 +15,29 @@ function App() {
   })
 
   const [update] = useMutation(UPDATE)
-  const onChangeNameHandler = (v: React.ChangeEvent<HTMLInputElement>) => {
-    setName(v.target.value);
+  // const onChangeNameHandler = (v: React.ChangeEvent<HTMLInputElement>) => {
+  //   setName(v.target.value);
+  // }
+
+  const onChangeNameHandler = (v: string) => {
+    console.log('name',v);
+    
+    setName(v)
   }
 
-  const onChangeDescHandler =  (v: React.ChangeEvent<HTMLInputElement>) => {
-    setDesc(v.target.value);
+  const onChangeDescHandler = (v: string) => {
+     console.log('desc',v);
+    setDesc(v) ;
   }
 
-  const onClickHandle = () => {
+  const onClickHandle = (v: any) => {
+    console.log(v,"????");
     update({
       variables: {
         id: '2000630e-4f1f-4d47-8bdc-bcf5b845a61a',
         params: {
-          id: '2000630e-4f1f-4d47-8bdc-bcf5b845a61a',
-          name,
-          desc,
+          // id: '2000630e-4f1f-4d47-8bdc-bcf5b845a61a',
+          ...v
         }
   }})
 }
@@ -38,15 +46,24 @@ function App() {
   return (<div><p>data:{JSON.stringify(data)}</p>
     <p>loading:{`${loading}`}</p>
     
-    <p>
+    {/* <p>
       name:
       <input type="text" onChange={onChangeNameHandler} />
     </p>
       <p>
       desc:
       <input type="text" onChange={onChangeDescHandler} />
-    </p>
-    <p><button onClick={onClickHandle}>更新</button></p>
+    </p> */} 
+    <Form layout='horizontal'  onFinish={onClickHandle} footer={(<Button block type='submit' color='primary' size='large'>提交</Button>)}>
+      <Form.Item name='name' label='姓名'
+        rules={[{ required: true, message: '姓名不能为空' }]}>
+          <Input onChange={onChangeNameHandler}/>
+      </Form.Item>
+            <Form.Item name='desc' label='描述'>
+        <Input onChange={onChangeDescHandler} />
+        </Form.Item>
+    </Form>
+    {/* <p><button onClick={onClickHandle}>更新</button></p> */}
   </div>)
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,16 @@ import { ApolloProvider } from '@apollo/client'
 import './index.css'
 import App from './App.tsx'
 import { client } from './utils/apollo.ts'
+import { ConfigProvider } from "antd-mobile";
+import zhCn from 'antd-mobile/es/locales/zh-Cn'
+
 
 createRoot(document.getElementById('root')!).render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+
+  <ConfigProvider locale={zhCn}>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+  </ConfigProvider>
+
 )
